36d4880bf7e08011fa0f4ee0ebb6d06757d6a1dd
Lizongcheng <1573479811@qq.com>
2019-08-29 20:20:06 +0800

    程序猿甲修改

--- git_demo/Hello/src/com/itheima/demo/HelloWorld.java
+++ git_demo/Hello/src/com/itheima/demo/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello ");
         System.out.println("程序猿乙修改内容");
+        System.out.println("dev");
     }
 }
